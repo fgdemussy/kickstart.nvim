add862b0fa5b28f5bf654b27b954653fcd3ca53e
Fran <fgdemussy@gmail.com>
2024-04-29 19:16:00 -0400

    loads plugins into init.lua

--- init.lua
+++ init.lua
@@ -245,6 +245,8 @@ require('lazy').setup({
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
 
+  -- lualine plugin
+
   -- Here is a more advanced example where we pass configuration
   -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
   --    require('gitsigns').setup({ ... })
