6549ac01d0ce546168e7c5cad31564fccb842ead
Fran <fgdemussy@gmail.com>
2024-04-29 19:15:46 -0400

    loads alpha, github copilot and lualine

--- init.lua
+++ init.lua
@@ -888,6 +888,9 @@ require('lazy').setup({
   -- require 'kickstart.plugins.lint',
   require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.lualine',
+  require 'kickstart.plugins.alpha',
+  require 'kickstart.plugins.github_copilot',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
