b5a15d53b2211f02c2092a939e08b604a6fcc75a
Fran <fgdemussy@gmail.com>
2024-07-09 07:38:17 -0400

    Adds vim-fugitive plugin

--- init.lua
+++ init.lua
@@ -895,7 +895,7 @@ require('lazy').setup({
   require 'kickstart.plugins.lualine',
   require 'kickstart.plugins.alpha',
   require 'kickstart.plugins.github_copilot',
-  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'kickstart.plugins.gitstuff', -- adds gitsigns recommend keymaps and vim-fugitive
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
